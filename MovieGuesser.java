aba05d9c2cff6e7dee2b210ddac0b4aef7581317
liraniS <52222143+liraniS@users.noreply.github.com>
2020-09-18 18:08:10 +0300

    add exceptions handling

--- MovieGuesser.java
+++ MovieGuesser.java
@@ -1,68 +1,72 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class MovieGuesser
 {
 
     public static void main(String [] args) throws Exception
     {
-        try {
-            File file = new File("movies.txt");
-            Scanner scanner = new Scanner(file);
+       try {
+           File file = new File("movies.txt");
+           Scanner scanner = new Scanner(file);           
+           String[] movies = new String[25];
+           int i=0;
+
+           while (scanner.hasNextLine()) {
+               movies[i] = (scanner.nextLine());
+               i++;
+            }
+            
+            int randNum = (int) (Math.random() * 25);
+            if (randNum == 25)
+                randNum--;
+            String chosenMovie = movies[randNum];
+        
+            System.out.println("Type a letter to guess the movie. 10 wrong letters means GAME OVER");
+        
+            String hidden = "";
+            for (int l = 0; l < chosenMovie.length(); l++) {
+                hidden += "_";
+            }
+            System.out.println(hidden);
+        
+            Scanner letterScanner = new Scanner(System.in);
+            String letter;
+            int index; 
+            int guess = 0;
+            String check;
+            while (guess < 10) {
+                letter = letterScanner.next();
+                if (letter.length()>1) {
+                    System.out.println("Print only a single english letter");
+                } else if (chosenMovie.contains(letter)) {
+                    System.out.println("correct");
+                    index = chosenMovie.indexOf(letter);
+                    while (index>=0) {
+                        hidden = hidden.substring(0, index) + letter + hidden.substring(index + 1);
+                        index = chosenMovie.indexOf(letter, index + 1);
+                    }                    
+                    System.out.println(hidden);
+                    check = hidden.replace("_"," ");
+                    if (check.contains(chosenMovie)) {
+                        System.out.println("You did it! Well done!");
+                        guess = 10;
+                    }
+                } else {
+                    guess++;
+                    System.out.println("wrong letter, this is try " + guess + " out of 10");
+                }
+            }   
+        
+            System.out.println("this movie was chosen: " + chosenMovie);
+        
         } catch (FileNotFoundException e){
             System.out.println("File missing!");
             System.exit(1);
-        }           
-        
-        
-        String[] movies = new String[25];
-        int i=0;
-
-        while (scanner.hasNextLine()) {
-            movies[i] = (scanner.nextLine());
-            i++;
+        } catch(Exception exception) {
+            System.out.println("An error had occured.");
+            System.exit(1);
         }
-        
-        int randNum = (int) (Math.random() * 25);
-        if (randNum == 25)
-            randNum--;
-        String chosenMovie = movies[randNum];
-        
-        System.out.println("Type a letter to guess the movie. 10 wrong letters means GAME OVER");
-        
-        String hidden = "";
-        for (int l = 0; l < chosenMovie.length(); l++) {
-            hidden += "_";
-        }
-        System.out.println(hidden);
-        
-        Scanner letterScanner = new Scanner(System.in);
-        String letter;
-        int index;
-        int guess = 0;
-        String check;
-        while (guess < 10) {
-            
-            letter = letterScanner.next();
-            if (chosenMovie.contains(letter)) {
-                System.out.println("correct");
-                index = chosenMovie.indexOf(letter);
-                while (index>=0) {
-                    hidden = hidden.substring(0, index) + letter + hidden.substring(index + 1);
-                    index = chosenMovie.indexOf(letter, index + 1);
-                }                    
-                System.out.println(hidden);
-                check = hidden.replace("_"," ");
-                if (check.contains(chosenMovie)) {
-                    System.out.println("You did it! Well done!");
-                    guess = 10;
-                }
-            } else {
-                guess++;
-                System.out.println("wrong letter, this is try " + guess + " out of 10");
-            }
-        }   
-        
-        System.out.println("this movie was chosen: " + chosenMovie);
     }
 }
